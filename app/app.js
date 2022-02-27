d1e3699ddeb747a792bd144e80092e2e9cf27080
AmrHesham <eng.amrhesham.22@gmail.com>
2022-02-27 19:33:34 +0200

    u

--- app/app.js
+++ app/app.js
@@ -6,12 +6,12 @@ const path = require('path')
 require("dotenv").config()
 require("../models/dbconnection/dbconnection")
 
-
-app.use(cors())
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://social-engine-frontend.herokuapp.com');
-    next();
-});
+const corsOptions ={
+    origin:'https://social-engine-frontend.herokuapp.com', 
+    credentials:true,      
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 

be4053413cc1c80e5b9ab4b9d37a2fb127af1e41
AmrHesham <eng.amrhesham.22@gmail.com>
2022-02-27 08:18:05 +0200

    de

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const app = require('./app/app')
 
-// use this line on your local machine
-//const PORT = process.env.PORT || 3000
-
-// use this line for heroku deployment
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT , () => {
-    console.log(`your project is running`)
+    console.log(`your project is running on ${PORT}`)
 })

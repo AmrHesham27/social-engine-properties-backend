f1e281e633f026ab2e563d0e9ce21c39faf1cc27
AmrHesham <eng.amrhesham.22@gmail.com>
2022-02-27 04:56:30 +0200

    de

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const app = require('./app/app')
-const PORT = process.env.PORT || 3000
+
+// use this line on your local machine
+//const PORT = process.env.PORT || 3000
+
+// use this line for heroku deployment
+const PORT = process.env.MONGODB_URI
+
 app.listen(PORT , () => {
     console.log(`your project is running on http://localhost:${PORT}`)
 })

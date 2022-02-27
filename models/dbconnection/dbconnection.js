45d04e09db147e8a29791d00e8f42daacb17f01f
AmrHesham <eng.amrhesham.22@gmail.com>
2022-02-27 06:33:24 +0200

    kk

--- models/dbconnection/dbconnection.js
+++ models/dbconnection/dbconnection.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose')
-try{mongoose.connect(process.env.DBURL)}
+
+// use this code to connect to mongo on your machine
+/* try{mongoose.connect(process.env.DBURL)}
 catch(e){
     console.log(e.message)
-}
+} */
+
+// use this code to connect to mongo by heroku
+try{ mongoose.connect( process.env.MONGODB_URI, {useNewUrlParser: true}) }
+catch(e){ console.error('Error while connecting to DB', e) }

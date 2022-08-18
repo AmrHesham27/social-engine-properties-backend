0de46ff48f82009b83dc3264bcdc98cdb7b3e281
AmrHesham <eng.amrhesham.22@gmail.com>
2022-08-18 04:30:26 +0200

    k

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ const express = require("express")
 const app = express()
 const cors = require('cors')
 const path = require('path')
-const corsOrigin = require('../middleware/cors')
 
 require("dotenv").config()
 require("../models/dbconnection/dbconnection")
@@ -18,7 +17,6 @@ app.use(express.urlencoded({extended:true}))
 
 const userRoutes = require("../routes/user.routes")
 app.use("/",userRoutes)
-app.all('*', corsOrigin);
 
 // path to get images 
 app.get('/images/:id/:ext', async(req,res)=>{
